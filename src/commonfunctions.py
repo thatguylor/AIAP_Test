676ef5fe3d5993adb62c0221308ce1a7b9be7c99
Yao Kuan <e0309659@u.nus.edu>
2022-01-14 18:38:10 +0800

    fixed ETL pipelines

--- src/commonfunctions.py
+++ src/commonfunctions.py
@@ -1,4 +1,6 @@
 import sqlite3
+import pandas as pd 
+import numpy as np 
 
 def ConnectToDB(pathtodb):
     try:
@@ -14,4 +16,38 @@ def ConnectToDB(pathtodb):
     except sqlite3.Error as error:
         print("Error while connecting to sqlite", error)
 
-    return sqliteConnection 
+    return sqliteConnection
+
+
+
+def DataCleaning(df,pathtosummary): 
+    df = df.drop_duplicates(subset ="ID", keep = 'first', inplace = True)
+
+    df.Survive = df.Survive.map({'Yes':1, 'No':0, '1':1,'0':0})
+
+    df.Gender = df.Gender.map({'Male':1,'Female':0})
+
+    df['Smoke'] = df['Smoke'].str.upper()  
+    df.Smoke = df.Smoke.map({'YES':1,'NO':0})
+
+    df = df[df.Age >0]  
+    df = df[df.Age < 100]
+
+    df['Ejection Fraction'] = df['Ejection Fraction'].map({'Low':'Low','Normal':'Normal','High':'High','L':'Low','N':'Normal'})
+
+    df = df.fillna(method="ffill")
+
+ ## Loggging summary to make sure ETL runs successfully 
+    file = open(pathtosummary,"r+")
+    file.truncate(0)
+    file.close()
+
+    for column in df:
+        file = open(pathtosummary,"a")
+        file.write(str(df[column].value_counts()))
+        file.write('\n')
+        file.write('='*20)
+        file.write('\n')
+        file.close()
+
+
